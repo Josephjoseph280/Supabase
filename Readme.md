f849c42200ba4f668e2fb49ff9bf5e317e498eea
astralcode-joseph <joseph@astralcodedynamics.com>
2024-05-24 15:19:44 +0530

    my first commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+This is a simple repo which uses the typescript and supabase library to fetch data from cloud supabse api
---
+++ calender.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const supabase_js_1 = require("@supabase/supabase-js");
+const supabase = (0, supabase_js_1.createClient)('https://mmomwaveurbmtooskpvw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tb213YXZldXJibXRvb3NrcHZ3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNjUzNjAyNiwiZXhwIjoyMDMyMTEyMDI2fQ.TpeQyq-6INqiQ54VcG604AGo5Or-MVx4plxwEJOdvZI');
+async function fetch() {
+    try {
+        const { data, error } = await supabase.from('Todo').select('*');
+        if (error)
+            console.log(error);
+        else
+            console.log("data is", data);
+    }
+    catch (error) {
+        console.log("error is somwthing is", error);
+    }
+}
+fetch();
+console.log("hello worked first");
